d9f2be033127dc8e0db0ce2f8ca5e2ffaea6b3c8
Kit Kogan <kkogan.dev@gmail.com>
2020-03-11 15:36:25 -0500

    authorization by clearance level completed

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -4,12 +4,19 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    console.log('is authenticated?', req. isAuthenticated())
+    console.log('in GET /secret')
+    if(req.isAuthenticated()){
+        pool.query(`SELECT * FROM "secret" WHERE "secrecy_level" < ${req.user.clearance_level};`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    } else {
+        res.sendStatus(403);
+    }
+    
 });
 
 module.exports = router;
